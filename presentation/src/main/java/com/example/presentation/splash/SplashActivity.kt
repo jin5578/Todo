ffa9ac4a164870c65eeefc7d752fef00ebf5a3db
HyeonSeong <jin55789@naver.com>
2024-04-29 21:44:58 +0900

    Init Splash

---
+++ presentation/src/main/java/com/example/presentation/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.presentation.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
