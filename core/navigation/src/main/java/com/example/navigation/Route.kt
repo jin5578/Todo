b1194244bdf1884b917bc692c10a67f8600ad27b
HyeonSeong <jin55789@naver.com>
2024-09-19 12:38:10 +0900

    Add navigation route

--- core/navigation/src/main/java/com/example/navigation/Route.kt
+++ core/navigation/src/main/java/com/example/navigation/Route.kt
@@ -5,4 +5,25 @@ import kotlinx.serialization.Serializable
 sealed interface Route {
     @Serializable
     data object Home : Route
+
+    @Serializable
+    data object Calendar : Route
+
+    @Serializable
+    data object AddTask : Route
+
+    @Serializable
+    data object CompletedTask : Route
+
+    @Serializable
+    data object IncompleteTask : Route
+
+    @Serializable
+    data object ThisWeekTask : Route
+
+    @Serializable
+    data object AllTask : Route
+
+    @Serializable
+    data object EditTask : Route
 }
